78466e8b72bfd025a4851fcdd927728683a167a0
yangale6 <alexanderj.yang@mail.utoronto.ca>
2018-11-20 21:00:16 -0500

    Created Template TileBoardManager Test class

---
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/TileBoardManagerTest.java
@@ -0,0 +1,54 @@
+package fall2018.csc2017.game_center.slidingtiles;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TileBoardManagerTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void getBoard() {
+    }
+
+    @Test
+    public void puzzleSolved() {
+    }
+
+    @Test
+    public void isValidTap() {
+    }
+
+    @Test
+    public void isSolvable() {
+    }
+
+    @Test
+    public void getInversion() {
+    }
+
+    @Test
+    public void touchMove() {
+    }
+
+    @Test
+    public void undo() {
+    }
+
+    @Test
+    public void hasUndo() {
+    }
+
+    @Test
+    public void getScore() {
+    }
+}
