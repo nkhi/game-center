888a6710eaf613dcbaf9bc39650624846b0e89a8
yangale6 <alexanderj.yang@mail.utoronto.ca>
2018-11-20 21:33:48 -0500

    Started isSolvableTest Added a BoardManager implementation method

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/TileBoardManagerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/TileBoardManagerTest.java
@@ -4,9 +4,61 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import fall2018.csc2017.game_center.R;
+
 import static org.junit.Assert.*;
 
 public class TileBoardManagerTest {
+    /**
+     * Solvable boardManager for testing.
+     */
+    TileBoardManager boardManagers;
+
+    /**
+     * Unsolvable boardManager for testing.
+     */
+    TileBoardManager boardManageru;
+
+    /**
+     * Make a solved board
+     */
+    private void setupCorrects(){
+        List<Tile> tiles = make4Tiles();
+        TileBoard board = new TileBoard(tiles, 4);
+        boardManagers = new TileBoardManager(board, 4, -1);
+    }
+
+    private void setupCorrectu(){
+        List<Tile> tiles = make4Tileu();
+        TileBoard board = new TileBoard(tiles, 4);
+        boardManageru = new TileBoardManager(board, 4, -1);
+    }
+    /**
+     * Solvable 4X4 board in list format for testing
+     */
+    private List<Tile> make4Tiles() {
+        List<Tile> tiles = new ArrayList<>();
+        final int numTiles = 16;
+        for (int tileNum = 0; tileNum != numTiles; tileNum++) {
+            tiles.add(new Tile(tileNum + 1, tileNum));
+        }
+        tiles.add(new Tile(numTiles, R.drawable.tile_25));
+
+        return tiles;
+    }
+
+    /**
+     * Unsolvable 4X4 board in list format for testing
+     */
+    private List <Tile> make4Tileu() {
+        List<Tile> tiles = make4Tiles();
+        Collections.swap(tiles, 10, 11);
+        return  tiles;
+    }
 
     @Before
     public void setUp() throws Exception {
@@ -30,6 +82,10 @@ public class TileBoardManagerTest {
 
     @Test
     public void isSolvable() {
+        setupCorrects();
+        setupCorrectu();
+        assertEquals(true, boardManagers.isSolvable());
+        assertEquals(false, boardManageru.isSolvable());
     }
 
     @Test
