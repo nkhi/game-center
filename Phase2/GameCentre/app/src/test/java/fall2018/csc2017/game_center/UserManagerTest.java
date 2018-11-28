038b97a7069034716c8c05a06773dd156a659a4b
yangale6 <alexanderj.yang@mail.utoronto.ca>
2018-11-28 13:14:10 -0500

    Test User and UserManager classes with no warnings

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/UserManagerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/UserManagerTest.java
@@ -21,7 +21,7 @@ public class UserManagerTest {
     }
 
     @After
-    public void tearDown() throws Exception {
+    public void tearDown(){
         user = null;
         userManager = null;
     }
