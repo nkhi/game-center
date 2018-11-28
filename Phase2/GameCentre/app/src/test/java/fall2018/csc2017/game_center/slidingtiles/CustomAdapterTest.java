cb4df572c601026070c5ad688f1b0def527a8712
shourya sharma <shourya.sharma@mail.utoronto.ca>
2018-11-28 06:45:34 -0500

    Test cases for custom adapter partially complete, the functionality is still in question. but in theory, the completed ones should work

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/CustomAdapterTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/CustomAdapterTest.java
@@ -1,17 +1,48 @@
 package fall2018.csc2017.game_center.slidingtiles;
 
+import android.widget.Button;
+
+import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.*;
 
 public class CustomAdapterTest {
+    private TileCustomAdapter
+
+    @Before
+    public void setup() {
+        ArrayList<Button> button = new ArrayList<>();
+        button.add(e);
+        button.add(x);
+        button.add(z);
+        TileCustomAdapter Testadpter = new TileCustomAdapter(button, 2, 3);
+
+
+    }
 
     @Test
     public void getCount() {
+        ArrayList<Button> button = new ArrayList<>();
+        button.add(e);
+        button.add(x);
+        button.add(z);
+        TileCustomAdapter Testadpter = new TileCustomAdapter(button,2,3);
+        assertEquals(3, Testadpter.getCount());
     }
 
     @Test
     public void getItem() {
+        ArrayList<Button> button = new ArrayList<>();
+        button.add(e);
+        button.add(x);
+        button.add(z);
+        TileCustomAdapter Testadpter = new TileCustomAdapter(button, 2, 3);
+        for (int i = 0; i < Testadpter.getCount();i++) {
+            assertEquals(button.get(i), Testadpter.getItem(i));
+        }
     }
 
     @Test
