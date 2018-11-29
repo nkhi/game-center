4f58a250aba9bc08739be9d4489c1bc1e159fd32
shourya sharma <shourya.sharma@mail.utoronto.ca>
2018-11-29 03:19:02 -0500

    Test cases for custom adapter complete, the functionality of get view is still in question because view goup in setup cannot be resolved. However that is the only issue that is visible

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/CustomAdapterTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/CustomAdapterTest.java
@@ -1,5 +1,9 @@
 package fall2018.csc2017.game_center.slidingtiles;
 
+import android.content.Context;
+import android.view.ContextThemeWrapper;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
 
 import org.junit.Before;
@@ -12,49 +16,69 @@ import fall2018.csc2017.game_center.R;
 import static org.junit.Assert.*;
 
 public class CustomAdapterTest {
-    private TileCustomAdapter
+    private TileCustomAdapter adapter;
+    private ArrayList<Button> buttons;
+    private View view;
+    private View view2;
+    private ViewGroup parent;
 
 
     @Before
     public void setup() {
-        ArrayList<Button> button = new ArrayList<>();
-        Button e;
-        e = findviewbyid(R.id.all);
+        final ArrayList<Button> button = new ArrayList<>();
+        Button e, x, z;
+        e = findViewById(R.id.all);
+        x = findViewById(R.id.NewGameButton);
+        z = findViewById(R.id.GameMenuTitle);
         button.add(e);
         button.add(x);
         button.add(z);
         TileCustomAdapter Testadpter = new TileCustomAdapter(button, 2, 3);
+        View v = null;
+        ViewGroup group = new ViewGroup() {
+            @Override
+            protected void onLayout(boolean b, int i, int i1, int i2, int i3) {
+                if(b) {button.get(i);}
+                else if(b != true) {button.get(i2);}
+            }
+        };
+        this.parent = group;
+        this.view = v;
+        this.view2 = e;
+        this.adapter = Testadpter;
+        this.buttons = button;
 
 
     }
 
     @Test
     public void getCount() {
-        ArrayList<Button> button = new ArrayList<>();
-        button.add(e);
-        button.add(x);
-        button.add(z);
-        TileCustomAdapter Testadpter = new TileCustomAdapter(button,2,3);
-        assertEquals(3, Testadpter.getCount());
+
+        assertEquals(3, adapter.getCount());
     }
 
     @Test
     public void getItem() {
-        ArrayList<Button> button = new ArrayList<>();
-        button.add(e);
-        button.add(x);
-        button.add(z);
-        TileCustomAdapter Testadpter = new TileCustomAdapter(button, 2, 3);
-        for (int i = 0; i < Testadpter.getCount();i++) {
-            assertEquals(button.get(i), Testadpter.getItem(i));
+                for (int i = 0; i < adapter.getCount();i++) {
+                    assertEquals(buttons.get(i), adapter.getItem(i));
         }
     }
 
     @Test
     public void getItemId() {
+        for(int i = 0; i < adapter.getCount(); i++) {
+            assertEquals(i, adapter.getItemId(i));
+        }
     }
 
     @Test
     public void getView() {
+        assertNull(view);
+        for(int i = 0; i < adapter.getCount();i++) {assertEquals(buttons.get(i), adapter.getView(i, view, parent));}
+        assertNotNull(view2);
+        for(int i = 0; i < adapter.getCount();i++) {assertEquals(view2, adapter.getView(i, view2, parent));}
+
+
+
     }
 }
