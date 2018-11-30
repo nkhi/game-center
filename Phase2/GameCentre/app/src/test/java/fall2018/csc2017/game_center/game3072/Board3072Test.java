464c311c7b337e8d71cd2d6e617f8b3984f07fd4
Anastasia <thecesi.zhang@mail.utoronto.ca>
2018-11-30 00:43:44 -0500

    Board3072 & Card 3072

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/game3072/Board3072Test.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/game3072/Board3072Test.java
@@ -3,13 +3,19 @@ package fall2018.csc2017.game_center.game3072;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.mockito.Mock;
 
 import static org.junit.Assert.*;
 
 public class Board3072Test {
 
+    @Mock
+    private Board3072 board1;
+
     @Test
     public void getBoard() {
+        assertEquals(board1.getBoard().length, 0);
+
     }
 
     @Test
