40abd207da311884b5284ad41d0021f45c3e74d5
Nikhil Bhambra <nikhil.bhambra@mail.utoronto.ca>
2018-11-29 21:54:50 -0500

    Created test files for Game3072 classes and files. Updated package dependancy for SavedGameStateTest.

---
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/game3072/Card3072Test.java
@@ -0,0 +1,30 @@
+package fall2018.csc2017.game_center.game3072;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class Card3072Test {
+
+    @Test
+    public void getNum() {
+    }
+
+    @Test
+    public void setNum() {
+    }
+
+    @Test
+    public void getCardWidth() {
+    }
+
+    @Test
+    public void equals() {
+    }
+
+    //@Test
+    //public void hashCode() {
+    //}
+}
---
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/game3072/MovementController3072Test.java
@@ -0,0 +1,38 @@
+package fall2018.csc2017.game_center.game3072;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class MovementController3072Test {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void setBoard() {
+    }
+
+    @Test
+    public void swipeRight() {
+    }
+
+    @Test
+    public void swipeLeft() {
+    }
+
+    @Test
+    public void swipeUp() {
+    }
+
+    @Test
+    public void swipeDown() {
+    }
+}
