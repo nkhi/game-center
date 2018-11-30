fc899846ac40aa7817fbb9c239b20f75ae7a8303
Nikhil Bhambra <nikhil.bhambra@mail.utoronto.ca>
2018-11-30 00:40:19 -0500

    Added Anastasia's original test code to master test file, will combine the test cases.

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/TileBoardTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/TileBoardTest.java
@@ -1,3 +1,48 @@
+package fall2018.csc2017.game_center.slidingtiles;
+
+import org.junit.Test;
+import org.junit.Before;
+import java.util.Iterator;
+import java.util.List;
+
+import static org.junit.Assert.*;
+
+public class TileBoardTest {
+
+    private TileBoard board;
+
+    //Not working
+    @Before
+    public void setup() {
+        this.board = new TileBoard(tiles, board.getNumRowCol());
+    }
+
+    @Test
+    public void numTiles() {
+        for (int i = 0; i < board.getNumRowCol(); i++) {
+            for (int j = 0; j < board.getNumRowCol(); j++) {
+                assertEquals(i*j, board.numTiles());
+            }
+        }
+    }
+
+    @Test
+    public void getTile() {
+        for (int i = 0; i < board.getNumRowCol(); i++) {
+            for (int j = 0; j < board.getNumRowCol(); j++) {
+                assertNotNull(board.getTile(i, j));
+            }
+        }
+    }
+
+
+    @Test
+    public void getNumRowCol(){
+        double num = Math.sqrt(board.numTiles());
+        assertEquals(num, board.getNumRowCol());
+    }
+}
+
 //package fall2018.csc2017.game_center.slidingtiles;
 //
 //import org.junit.After;
