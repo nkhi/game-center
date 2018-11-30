a4d99ca0235ebd15f53d1e1701b3a57683b9b54a
shourya sharma <shourya.sharma@mail.utoronto.ca>
2018-11-29 20:24:17 -0500

    first test for movement controller

--- Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/MovementControllerTest.java
+++ Phase2/GameCentre/app/src/test/java/fall2018/csc2017/game_center/slidingtiles/MovementControllerTest.java
@@ -1,17 +1,50 @@
 package fall2018.csc2017.game_center.slidingtiles;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.*;
 
 public class MovementControllerTest {
+    private TileBoardManager manager;
+    private TileBoardManager manage2;
+
+    @Before
+    public void setup() {
+        TileBoardManager managerTest = new TileBoardManager(4,3);
+        TileBoardManager managerTest2 = new TileBoardManager(5,  -1);
+        this.manager = managerTest;
+        this.manage2 = managerTest2;
+
+
+    }
 
     @Test
     public void setBoardManager() {
+        assertTrue(manager.hasUndo() == true);
+        int y = 0;
+        for( int i = 0; i < manager.getBoard().getNumRowCol();i++) {
+            if(manager.isValidTap(i)){
+                manager.touchMove(i);
+                y++;
+                if(y > 4) {break;}
+            }
+        }
+        manager.undo();
+        manager.undo();
+        manager.undo();
+        assertFalse(manager.hasUndo() != true);
+        assertTrue(manager.getBoard().getNumRowCol() == 16);
+
+
+
+
     }
 
     @Test
     public void processTapMovement() {
+
+
     }
 
     @Test
